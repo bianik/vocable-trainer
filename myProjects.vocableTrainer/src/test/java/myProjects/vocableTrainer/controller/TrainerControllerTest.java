85933179f609fdcad29bbf9987901a334d089f66
Niklas Fabian <niklas.fabian@rub.de>
2019-06-15 18:49:26 +0200

    added TrainerControllerTest.java, .travis.yml

---
+++ myProjects.vocableTrainer/src/test/java/myProjects/vocableTrainer/controller/TrainerControllerTest.java
@@ -0,0 +1,30 @@
+package myProjects.vocableTrainer.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import myProjects.vocableTrainer.repository.VocableRepository;
+import myProjects.vocableTrainer.view.TrainerView;
+
+public class TrainerControllerTest {
+
+	@Mock
+	private TrainerView trainerView;
+	
+	@Mock
+	private VocableRepository vocableRepositor;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
