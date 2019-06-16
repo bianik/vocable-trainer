5425f00a8089af7d3375b8dd349bf2d732a75e2c
Niklas Fabian <niklas.fabian@rub.de>
2019-06-17 01:36:49 +0200

    TDD on TrainerController: added constructor -test passes-

--- myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/controller/TrainerController.java
+++ myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/controller/TrainerController.java
@@ -1,11 +1,18 @@
 package myProjects.vocableTrainer.controller;
 
 import myProjects.vocableTrainer.model.Vocable;
+import myProjects.vocableTrainer.repository.VocableRepository;
 
 public class TrainerController {
-
+	private VocableRepository vocableRepository;
+	
+	public TrainerController(VocableRepository vocableRepository) {
+		super();
+		this.vocableRepository = vocableRepository;
+	}
+	
 	public void newVocable(Vocable voc) {
-		
+		vocableRepository.findByPhrase(voc.getPhrase());
 	}
 
 }
