106bd1631eb94715348a00070039c8e4533384bc
Niklas Fabian <niklas.fabian@rub.de>
2019-06-17 12:41:47 +0200

    TDD on TrainerController: -test passes-

--- myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/controller/TrainerController.java
+++ myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/controller/TrainerController.java
@@ -2,18 +2,22 @@ package myProjects.vocableTrainer.controller;
 
 import myProjects.vocableTrainer.model.Vocable;
 import myProjects.vocableTrainer.repository.VocableRepository;
+import myProjects.vocableTrainer.view.TrainerView;
 
 public class TrainerController {
 	private VocableRepository vocableRepository;
+	private TrainerView trainerView;
 	
-	public TrainerController(VocableRepository vocableRepository) {
+	public TrainerController(VocableRepository vocableRepository, TrainerView trainerView) {
 		super();
 		this.vocableRepository = vocableRepository;
+		this.trainerView = trainerView;
 	}
 	
 	public void newVocable(Vocable voc) {
 		vocableRepository.findByPhrase(voc.getPhrase());
 		vocableRepository.saveVocable(voc);
+		trainerView.showMessageVocableAdded("Vocable added", voc);
 	}
 
 }
