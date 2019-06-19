ec7b78cf763941f921af25eb1bdccc37f0ac6ff4
Niklas Fabian <niklas.fabian@rub.de>
2019-06-20 00:36:35 +0200

    UT: refactored  SUT -test passes-

--- myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/repository/h2/H2VocableRepository.java
+++ myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/repository/h2/H2VocableRepository.java
@@ -19,33 +19,15 @@ public class H2VocableRepository implements VocableRepository {
 	}
 
 	public Vocable findByPhrase(String phrase) {
-		String command = "SELECT * FROM " + tableName + " WHERE PHRASE = '" + phrase + "'";
-		Statement stmt = null;
-		ResultSet rs =  null;
-		Vocable v = null;
-		try {
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery(command);
-	         // extract data from result set 
-	         if(rs.first()) { 
-	        	v = new Vocable();
-	            // Retrieve by column name 
-	            v.setPhrase(rs.getString("phrase"));
-	            v.setTranslation(rs.getString("translation"));
-	            v.setCorrTries(rs.getInt("corrTries"));
-	            v.setFalseTries(rs.getInt("falseTries"));
-	         }
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {if(rs != null) rs.close();} catch (SQLException e) {}
-			try {if(stmt != null) stmt.close();} catch (SQLException e) {}
-		}
-		return v;
+		return findBy("PHRASE", phrase);
 	}
 
 	public Vocable findByTranslation(String translation) {
-		String command = "SELECT * FROM " + tableName + " WHERE TRANSLATION = '" + translation + "'";
+		return findBy("TRANSLATION", translation);
+	}
+	
+	private Vocable findBy(String column, String argument) {
+		String command = "SELECT * FROM " + tableName + " WHERE " + column + " = '" + argument + "'";
 		Statement stmt = null;
 		ResultSet rs =  null;
 		Vocable v = null;
