2891d7422c609f502f76dc1bd8773804a0a23468
Niklas Fabian <niklas.fabian@rub.de>
2019-06-20 00:27:34 +0200

    UT: -test passes-

--- myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/repository/h2/H2VocableRepository.java
+++ myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/repository/h2/H2VocableRepository.java
@@ -45,8 +45,29 @@ public class H2VocableRepository implements VocableRepository {
 	}
 
 	public Vocable findByTranslation(String translation) {
-		// TODO Auto-generated method stub
-		return null;
+		String command = "SELECT * FROM " + tableName + " WHERE TRANSLATION = '" + translation + "'";
+		Statement stmt = null;
+		ResultSet rs =  null;
+		Vocable v = null;
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(command);
+	         // extract data from result set 
+	         if(rs.first()) { 
+	        	v = new Vocable();
+	            // Retrieve by column name 
+	            v.setPhrase(rs.getString("phrase"));
+	            v.setTranslation(rs.getString("translation"));
+	            v.setCorrTries(rs.getInt("corrTries"));
+	            v.setFalseTries(rs.getInt("falseTries"));
+	         }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {if(rs != null) rs.close();} catch (SQLException e) {}
+			try {if(stmt != null) stmt.close();} catch (SQLException e) {}
+		}
+		return v;
 	}
 
 	public void saveVocable(Vocable vocable) {
