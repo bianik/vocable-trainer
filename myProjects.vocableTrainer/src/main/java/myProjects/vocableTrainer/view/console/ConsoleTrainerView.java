28e92b5e7920d150d5feed7f2854dfda725ba14b
Niklas Fabian <niklas.fabian@rub.de>
2019-06-20 23:10:26 +0200

    initial commit for branch unit_tests_ConsoleView

---
+++ myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/view/console/ConsoleTrainerView.java
@@ -0,0 +1,26 @@
+package myProjects.vocableTrainer.view.console;
+
+import myProjects.vocableTrainer.model.Vocable;
+import myProjects.vocableTrainer.view.TrainerView;
+
+public class ConsoleTrainerView implements TrainerView {
+
+	@Override
+	public void showMessageVocableAdded(String message, Vocable vocable) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showCheckResult(String message, boolean result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showNextVocable(String message, Vocable vocable) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
