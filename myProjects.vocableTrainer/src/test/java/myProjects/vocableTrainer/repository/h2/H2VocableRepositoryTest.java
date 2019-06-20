58353ff7164a7c7622b09fb47249632c095a5494
Niklas Fabian <niklas.fabian@rub.de>
2019-06-20 02:53:29 +0200

    UT: added test -test fails-

--- myProjects.vocableTrainer/src/test/java/myProjects/vocableTrainer/repository/h2/H2VocableRepositoryTest.java
+++ myProjects.vocableTrainer/src/test/java/myProjects/vocableTrainer/repository/h2/H2VocableRepositoryTest.java
@@ -132,6 +132,18 @@ public class H2VocableRepositoryTest {
 		assertThat(nextVocable).isEqualTo(secondVocable);
 	}
 	
+	@Test
+	public void testNextVocableWhenCurrentVocableLastOne() {
+		// setup
+		Vocable firstVocable = addTestVocable("phrase 1", "translation 1", 0, 0);
+		Vocable secondVocable = addTestVocable("phrase 2", "translation 2", 0, 0);
+		Vocable lastVocable = addTestVocable("phrase 3", "translation 3", 0, 0);
+		// execution
+		Vocable nextVocable = vocableRepo.nextVocable(lastVocable);
+		// verify
+		assertThat(nextVocable).isEqualTo(firstVocable);
+	}
+	
 	////////////////// helping functions ////////////////////////////////
 	
 	private List<Vocable> readAllVocablesFromRepository() {
