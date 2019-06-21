a4227c1ff959bdd01cfb41a0acfb15be0c399e99
Niklas Fabian <niklas.fabian@rub.de>
2019-06-21 13:40:26 +0200

    UT: -test passes-

--- myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/view/console/ConsoleTrainerView.java
+++ myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/view/console/ConsoleTrainerView.java
@@ -39,6 +39,8 @@ public class ConsoleTrainerView implements TrainerView {
 
 	public void startConsole() {
 		out.println("##### Vocable Trainer #####\nenter 'n'/'new' to add a new vocable\nenter 'l'/'learn' to start learning");
+		if(in.hasNextLine())
+			out.println("phrase: ");
 	}
 
 }
