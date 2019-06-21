16d53cf835881784c5b0165e85f95867f3b394ce
Niklas Fabian <niklas.fabian@rub.de>
2019-06-21 17:53:41 +0200

    UT: -test passes-

--- myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/view/console/ConsoleTrainerView.java
+++ myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/view/console/ConsoleTrainerView.java
@@ -13,6 +13,12 @@ public class ConsoleTrainerView implements TrainerView {
 	private TrainerController trainerContr;
 	private Vocable currentVocable = null;
 	private boolean startMessage = true;
+	
+	// ANSI escape codes for colors
+	private static final String ANSI_RESET = "\u001B[0m";
+	private static final String ANSI_RED = "\u001B[31m";
+	private static final String ANSI_GREEN = "\u001B[32m";
+	private static final String ANSI_YELLOW = "\u001B[33m";
 
 	public ConsoleTrainerView(Scanner in, PrintStream out, TrainerController trainerContr) {
 		super();
@@ -28,7 +34,7 @@ public class ConsoleTrainerView implements TrainerView {
 
 	@Override
 	public void showCheckResult(String message, boolean result) {
-		out.println(message);
+		out.println(ANSI_GREEN + message+ ANSI_RESET);
 	}
 
 	@Override
