29ac4b574f62cd405e15db244706e030bf7ff242
Niklas Fabian <niklas.fabian@rub.de>
2019-06-23 16:28:48 +0200

    UT: -test passes-

--- myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/view/swing/SwingTrainerView.java
+++ myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/view/swing/SwingTrainerView.java
@@ -272,6 +272,7 @@ public class SwingTrainerView extends JFrame implements TrainerView {
 	@Override
 	public void showNextVocable(String message, Vocable vocable) {
 		lblShow.setText(vocable.getTranslation());
+		this.currentVocable = vocable;
 	}
 
 	public void setTrainerController(TrainerController trainerController) {
