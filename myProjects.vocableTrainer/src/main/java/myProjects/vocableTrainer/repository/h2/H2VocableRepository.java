d0c6276eef7cf8ce0e7509373510a0adfb380c43
Niklas Fabian <niklas.fabian@rub.de>
2019-06-25 00:41:45 +0200

    UT: -test passes-

--- myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/repository/h2/H2VocableRepository.java
+++ myProjects.vocableTrainer/src/main/java/myProjects/vocableTrainer/repository/h2/H2VocableRepository.java
@@ -19,7 +19,7 @@ public class H2VocableRepository implements VocableRepository {
 	}
 
 	public void initialize() {
-		String command = "CREATE TABLE " + tableName + "(phrase VARCHAR(30))";
+		String command = "CREATE TABLE " + tableName + "(phrase VARCHAR(30), translation VARCHAR(30))";
 		try (Statement stmt = conn.createStatement();) {
 			stmt.executeUpdate(command);
 		} catch (SQLException e) {
